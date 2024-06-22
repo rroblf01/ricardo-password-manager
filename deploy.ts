6f01f266c3d2a289bacf9622ff5f360b9fced826
Ricardo Robles <rroblf01@estudiantes.unileon.es>
2024-06-22 12:09:38 +0200

    chore: Update Deno Deploy configuration and entrypoint for deployment

---
+++ deploy.ts
@@ -0,0 +1,13 @@
+import { serveDir } from "https://deno.land/std@0.217.0/http/file_server.ts";
+
+Deno.serve(async (req) => {
+  const fsRoot = "dist/ricardo-password-manager/browser";
+  const headers = ["Cache-Control: max-age=31536000"];
+  const response = await serveDir(req, { fsRoot, headers });
+  if (response.status === 404) {
+    const url = new URL(req.url);
+      return Response.redirect(url.origin)
+  }
+  return response;
+  
+});
